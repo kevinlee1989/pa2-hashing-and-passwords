136929b63fce993ab3b12f4e43f36c599fe605af
Lee <kjl015@ieng6-203.ucsd.edu>
2024-10-22 12:30:47 -0700

    KevinKeivn

--- pwcrack.c
+++ pwcrack.c
@@ -183,7 +183,7 @@ int main(int argc, char **argv){
     assert(match == 1);
     assert(password[2] == 's'); // the uppercase 'S' has been lowercased
 */
-
+//Making a Change in ieng6 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
    unsigned char given_hash[32];
     hexstr_to_hash(argv[1], given_hash);
 
